9e307cbfca1cc7adb28e1eadfa34e3f6f5fa1a52
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-10-10 15:13:22 +0200

    Add missing packag files, move code to one module

--- Data/Text/Binary.hs
+++ Data/Text/Binary.hs
@@ -1,10 +1,18 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Data.Text.Binary where
 
 import Control.Applicative ((<$>))
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+import qualified Data.Text.Lazy as L
+import qualified Data.Text.Lazy.Encoding as L
 import Data.Binary
 
 instance Binary T.Text where
     put = put . T.encodeUtf8
     get = T.decodeUtf8 <$> get
+
+instance Binary L.Text where
+    put = put . L.encodeUtf8
+    get = L.decodeUtf8 <$> get
